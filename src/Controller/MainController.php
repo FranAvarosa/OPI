3637d33d1de6e87b6ee91ac8c4c5e8fd48ebc669
Depresln <depresles.nicolas@gmail.com>
2022-03-01 16:55:58 +0100

    save 01/03

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/calendar')]
+#[Route('/cal')]
 class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
