cde02e31dd16ea6dc99df9bb4547093aed6835d1
Depresln <depresles.nicolas@gmail.com>
2022-03-08 12:10:35 +0100

    made non identified users unable to see pages with correct redirection

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -7,6 +7,7 @@ use App\Form\CalendarType;
 use App\Repository\CalendarRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,73 +19,11 @@ use function Symfony\Component\String\toString;
 class CalendarController extends AbstractController
 {
     #[Route('/new', name: 'calendar_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, CalendarRepository $calendar): Response
     {
-
-        $calendar = new Calendar();
-        $form = $this->createForm(CalendarType::class, $calendar);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $date1 = $calendar->getStart();
-
-            $date1Ymd = $calendar->getStart()->format('Y-m-d');
-            $date2His = $calendar->getEnd()->format('H:i:s');
-            $dateString = $date1Ymd . ' ' . $date2His;
-            $dateEnd = date_create_from_format('Y-m-d H:i:s', $dateString);
-
-            if($dateEnd > $date1){
-                $category = $calendar->getCategory();
-                switch($category) {
-                    case "En attente";
-                        $calendar->setBackgroundColor('#b7b7b7');
-                        break;
-                    case "Travail de rue";
-                        $calendar->setBackgroundColor('#eac159');
-                        break;
-                    case "Travail de nuit";
-                        $calendar->setBackgroundColor('#bf82dd');
-                        break;
-                    default:
-                        $calendar->setBackgroundColor("#b7b7b7");
-                }
-
-                $calendar->setUser($this->getUser());
-                $calendar->setEnd($dateEnd);
-                $entityManager->persist($calendar);
-                $entityManager->flush();
-            } else {
-                return $this->redirectToRoute('404', [], Response::HTTP_SEE_OTHER);
-            }
-
-            return $this->redirectToRoute('main', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('calendar/new.html.twig', [
-            'calendar' => $calendar,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'calendar_show', methods: ['GET'])]
-    public function show(Calendar $calendar): Response
-    {
-        return $this->render('calendar/show.html.twig', [
-            'calendar' => $calendar,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'calendar_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Calendar $calendar, EntityManagerInterface $entityManager): Response
-    {
-        $currentUserId = $this->getUser()->getId();
-        $currentUserService = $this->getUser()->getService();
-        $attachedUserId = $calendar->getUser()->getId();
-        $attachedUserService = $calendar->getUser()->getService();
-        $roleCheckerChef = $this->container->get('security.authorization_checker')->isGranted('ROLE_CHEFSERVICE');
-        $roleCheckerAdmin = $this->container->get('security.authorization_checker')->isGranted('ROLE_ADMIN');
-
-        if($roleCheckerAdmin or $currentUserId == $attachedUserId or $currentUserService == $attachedUserService and $roleCheckerChef) {
+        $securityContext = $this->container->get('security.authorization_checker');
+        if ($securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $calendar = new Calendar();
             $form = $this->createForm(CalendarType::class, $calendar);
             $form->handleRequest($request);
 
@@ -99,66 +38,129 @@ class CalendarController extends AbstractController
                 if ($dateEnd > $date1) {
                     $category = $calendar->getCategory();
                     switch ($category) {
-                        case "En attente":
-                            $calendar->setBackgroundColor('#CCCCFF');
+                        case "En attente";
+                            $calendar->setBackgroundColor('#b7b7b7');
                             break;
-                        case "Arrêt Maladie":
-                        case "Jour férié":
-                        case "CT":
-                        case "CA":
-                        case "Absence":
-                        case "Action Institution et partenariat":
-                        case "Présence sociale":
-                            $calendar->setBackgroundColor('#FFCCCC');
+                        case "Travail de rue";
+                            $calendar->setBackgroundColor('#eac159');
                             break;
-                        case "DP":
-                            $calendar->setBackgroundColor('#FF9900');
-                            break;
-                        case "TA COMPT":
-                            $calendar->setBackgroundColor('#6666CC');
-                            break;
-                        case "AEP":
-                            $calendar->setBackgroundColor('#9999FF');
-                            break;
-                        case "Evaluation":
-                            $calendar->setBackgroundColor('#FF6666');
-                            break;
-                        case "Formation":
-                            $calendar->setBackgroundColor('#33CCFF');
-                            break;
-                        case "Coordination et préparation":
-                            $calendar->setBackgroundColor('#CC9933');
-                            break;
-                        case "Animation éducative et sociale":
-                            $calendar->setBackgroundColor('#ECE9D8');
-                            break;
-                        case "Travail de rue":
-                            $calendar->setBackgroundColor('#00CCCC');
-                            break;
-                        case "Présence sociale hors local":
-                            $calendar->setBackgroundColor('#99CFD8');
+                        case "Travail de nuit";
+                            $calendar->setBackgroundColor('#bf82dd');
                             break;
                         default:
-                            $calendar->setBackgroundColor("#CCCCFF");
+                            $calendar->setBackgroundColor("#b7b7b7");
                     }
 
+                    $calendar->setUser($this->getUser());
                     $calendar->setEnd($dateEnd);
                     $entityManager->persist($calendar);
                     $entityManager->flush();
                 } else {
-                    return $this->redirectToRoute('calendar_edit', [], Response::HTTP_SEE_OTHER);
+                    return $this->redirectToRoute('404', [], Response::HTTP_SEE_OTHER);
                 }
 
                 return $this->redirectToRoute('main', [], Response::HTTP_SEE_OTHER);
             }
+
+            return $this->renderForm('calendar/new.html.twig', [
+                'calendar' => $calendar,
+                'form' => $form,
+            ]);
         } else {
             return $this->render('security/restricted.html.twig');
         }
+    }
 
-        return $this->renderForm('calendar/edit.html.twig', [
-            'calendar' => $calendar,
-            'form' => $form,
-        ]);
+    #[Route('/{id}/edit', name: 'calendar_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Calendar $calendar, EntityManagerInterface $entityManager): Response
+    {
+        $securityContext = $this->container->get('security.authorization_checker');
+        if ($securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $currentUserId = $this->getUser()->getId();
+            $currentUserService = $this->getUser()->getService();
+            $attachedUserId = $calendar->getUser()->getId();
+            $attachedUserService = $calendar->getUser()->getService();
+            $roleCheckerChef = $this->container->get('security.authorization_checker')->isGranted('ROLE_CHEFSERVICE');
+            $roleCheckerAdmin = $this->container->get('security.authorization_checker')->isGranted('ROLE_ADMIN');
+
+            if ($roleCheckerAdmin or $currentUserId == $attachedUserId or $currentUserService == $attachedUserService and $roleCheckerChef) {
+                $form = $this->createForm(CalendarType::class, $calendar);
+                $form->handleRequest($request);
+
+                if ($form->isSubmitted() && $form->isValid()) {
+                    $date1 = $calendar->getStart();
+
+                    $date1Ymd = $calendar->getStart()->format('Y-m-d');
+                    $date2His = $calendar->getEnd()->format('H:i:s');
+                    $dateString = $date1Ymd . ' ' . $date2His;
+                    $dateEnd = date_create_from_format('Y-m-d H:i:s', $dateString);
+
+                    if ($dateEnd > $date1) {
+                        $category = $calendar->getCategory();
+                        switch ($category) {
+                            case "En attente":
+                                $calendar->setBackgroundColor('#CCCCFF');
+                                break;
+                            case "Arrêt Maladie":
+                            case "Jour férié":
+                            case "CT":
+                            case "CA":
+                            case "Absence":
+                            case "Action Institution et partenariat":
+                            case "Présence sociale":
+                                $calendar->setBackgroundColor('#FFCCCC');
+                                break;
+                            case "DP":
+                                $calendar->setBackgroundColor('#FF9900');
+                                break;
+                            case "TA COMPT":
+                                $calendar->setBackgroundColor('#6666CC');
+                                break;
+                            case "AEP":
+                                $calendar->setBackgroundColor('#9999FF');
+                                break;
+                            case "Evaluation":
+                                $calendar->setBackgroundColor('#FF6666');
+                                break;
+                            case "Formation":
+                                $calendar->setBackgroundColor('#33CCFF');
+                                break;
+                            case "Coordination et préparation":
+                                $calendar->setBackgroundColor('#CC9933');
+                                break;
+                            case "Animation éducative et sociale":
+                                $calendar->setBackgroundColor('#ECE9D8');
+                                break;
+                            case "Travail de rue":
+                                $calendar->setBackgroundColor('#00CCCC');
+                                break;
+                            case "Présence sociale hors local":
+                                $calendar->setBackgroundColor('#99CFD8');
+                                break;
+                            default:
+                                $calendar->setBackgroundColor("#CCCCFF");
+                        }
+
+                        $calendar->setEnd($dateEnd);
+                        $entityManager->persist($calendar);
+                        $entityManager->flush();
+                    } else {
+                        return $this->redirectToRoute('calendar_edit', [], Response::HTTP_SEE_OTHER);
+                    }
+
+                    return $this->redirectToRoute('main', [], Response::HTTP_SEE_OTHER);
+                }
+            } else {
+                return $this->render('security/restricted.html.twig');
+            }
+
+            return $this->renderForm('calendar/edit.html.twig', [
+                'calendar' => $calendar,
+                'form' => $form,
+            ]);
+        } else {
+            return $this->render('security/restricted.html.twig');
+        }
     }
 
     #[Route('/{id}', name: 'calendar_delete', methods: ['POST'])]
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,6 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Service\ValidatorService;
 use App\Entity\User;
 use PDO;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use function Symfony\Component\Mime\toString;
 
 #[Route('/cal')]
@@ -21,28 +22,33 @@ class MainController extends AbstractController
     #[Route('/', name: 'main')]
     public function index(CalendarRepository $calendar, UserRepository $userRepository): Response
     {
-        $userId = $this->getUser()->getId();
-        $service = $this->getUser()->getService();
-        $events = $calendar->findBy(['User' => $userId]);
+        $securityContext = $this->container->get('security.authorization_checker');
+        if ($securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $userId = $this->getUser()->getId();
+            $service = $this->getUser()->getService();
+            $events = $calendar->findBy(['User' => $userId]);
 
-        $planning = [];
-        foreach($events as $event){
-            $planning[] = [
-                'id' => $event->getId(),
-                'start' => $event->getStart()->format('Y-m-d H:i:s'),
-                'end' => $event->getEnd()->format('Y-m-d H:i:s'),
-                'title' => $event->getTitle(),
-                'description' => $event->getDescription(),
-                'backgroundColor' => $event->getBackgroundColor(),
-                'category' => $event->getCategory(),
-            ];
+            $planning = [];
+            foreach($events as $event){
+                $planning[] = [
+                    'id' => $event->getId(),
+                    'start' => $event->getStart()->format('Y-m-d H:i:s'),
+                    'end' => $event->getEnd()->format('Y-m-d H:i:s'),
+                    'title' => $event->getTitle(),
+                    'description' => $event->getDescription(),
+                    'backgroundColor' => $event->getBackgroundColor(),
+                    'category' => $event->getCategory(),
+                ];
+            }
+
+            return $this->render('main/index.html.twig', [
+                'planning' => $planning,
+                'list' => $userRepository->findAll(),
+                'userService' => $service,
+            ]);
+        } else {
+            return $this->render('security/restricted.html.twig');
         }
-
-        return $this->render('main/index.html.twig', [
-            'planning' => $planning,
-            'list' => $userRepository->findAll(),
-            'userService' => $service,
-        ]);
     }
 
     #[Route('/admin/', name: 'main_admin', methods: ['GET'])]
