d266c7bf8f5d0c87e352965167bcb65cd8a9ffd6
MasaFukawa <elghazimarwanepro@gmail.com>
2022-03-09 16:39:00 +0100

    ajout service1 et 2

--- migrations/Version20220309153820.php
+++ migrations/Version20220309153820.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220309085001 extends AbstractMigration
+final class Version20220309153820 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,13 @@ final class Version20220309085001 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('ALTER TABLE user CHANGE service2 service2 VARCHAR(100) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE calendar CHANGE title title VARCHAR(100) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE description description LONGTEXT NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE background_color background_color VARCHAR(7) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE category category VARCHAR(100) NOT NULL COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('ALTER TABLE user CHANGE email email VARCHAR(180) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE password password VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE prenom prenom VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE nom nom VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE service service VARCHAR(100) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE user CHANGE email email VARCHAR(180) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE password password VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE prenom prenom VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE nom nom VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE service1 service1 VARCHAR(100) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE service2 service2 VARCHAR(100) CHARACTER SET utf8 DEFAULT NULL COLLATE `utf8_unicode_ci`');
     }
 }
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
@@ -20,10 +21,28 @@ class UserCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')->hideOnForm(),
             EmailField::new('email'),
             TextEditorField::new('nom'),
             TextEditorField::new('prenom'),
+            ChoiceField::new('service1', 'Service')->setChoices([
+                'Quartier De Neuhof' => 'Quartier De Neuhof',
+                'Quartier Cité de l\'Ill' => 'Quartier Cité de l\'Ill',
+                'Quartier De Koenigshoffen' => 'Quartier De Koenigshoffen',
+                'Quartier Montagne Verte' => 'Quartier Montagne Verte',
+                'Quartier Ampère - Port du Rhin' => 'Quartier Ampère - Port du Rhin',
+                'Quartier De l\'Elsau' => 'Quartier De l\'Elsau',
+                'Equipe Focale' => 'Equipe Focale',
+            ]),
+            ChoiceField::new('service2', 'Service')->setChoices([
+                'Quartier De Neuhof' => 'Quartier De Neuhof',
+                'Quartier Cité de l\'Ill' => 'Quartier Cité de l\'Ill',
+                'Quartier De Koenigshoffen' => 'Quartier De Koenigshoffen',
+                'Quartier Montagne Verte' => 'Quartier Montagne Verte',
+                'Quartier Ampère - Port du Rhin' => 'Quartier Ampère - Port du Rhin',
+                'Quartier De l\'Elsau' => 'Quartier De l\'Elsau',
+                'Equipe Focale' => 'Equipe Focale',
+            ]),
         ];
     }
     
